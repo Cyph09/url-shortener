890229f527fa170ba12147f5f945d1f530efa217
Swaleh Mhina <cyph09@gmail.com>
2019-10-22 10:27:36 +0300

    Base url to remote url

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.MONGODB_URI ||
     "mongodb+srv://mhina:mhina123@cluster0-s4yha.mongodb.net/test?retryWrites=true&w=majority",
   // BASE_URL: "http://localhost:5000"
-  BASE_URL: "https://infinite-reef-65688.herokuapp.com/"
+  BASE_URL: "https://infinite-reef-65688.herokuapp.com"
   //   const production  = 'https://examplePage.com';
   // const development = 'http://localhost:3000/';
   // const url = (process.env.NODE_ENV ? production : development);
